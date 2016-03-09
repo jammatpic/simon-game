42a92f3ed844d9d460164840e21c93bf0fad74c9
jammatpic <jammatpic@gmail.com>
2016-03-09 20:32:16 +1100

    added basic HTML

--- resources/js/simon-game.js
+++ resources/js/simon-game.js
@@ -1,3 +1,22 @@
+function displayPattern() {
+    for (i = 0; i < pattern.length; i++) {
+        
+    }
+}
+
 $(document).ready(function() {
-   
+    var pattern = [];
+    gameStart = false;
+    nextMove = Math.floor(Math.random()*4)
+    $("btn-play").on("click", function() {
+        pattern = [];
+        gameStart = true;
+        nextMove = Math.floor(Math.random()*4)
+    });
+    if (gameStart == true) {
+        $("btn-main").on("click", function() {
+            
+        });
+    }
+    
 });
