0817ab6728a55937d80a797aa2d35ed469a00dfc
James <jammatpic@gmail.com>
2016-03-10 01:13:07 +1100

    Added Basic Gameplay

--- resources/js/simon-game.js
+++ resources/js/simon-game.js
@@ -1,22 +1,55 @@
-function displayPattern() {
+var pattern = []; 
+var buttons = ["btn-green", "btn-red", "btn-yellow", "btn-blue"];
+var clickPattern = [];
+
+// displays the pattern on the screen through simulated button presses
+function displayPattern(i) {
+    var button = "#" + buttons[pattern[i]];
+    setTimeout(function() {
+        $(button).toggleClass(button.slice(1, button.length) + "-press");
+    }, (i*2+1)*1000); // used to time every toggle every 1000 milliseconds
+    setTimeout(function() {
+        $(button).toggleClass(button.slice(1, button.length) + "-press");
+    }, (i*2+2)*1000); 
+}
+
+// adds an element to a pattern and calls displayPattern
+function addToPattern() {
+    nextMove = Math.floor(Math.random()*4)
+    pattern.push(nextMove);
     for (i = 0; i < pattern.length; i++) {
-        
+        displayPattern(i);
     }
 }
 
+function areArraysEqual(a, b) {
+    if (a.length != b.length) {
+        return false;
+    }
+    for (i = 0; i < a.length; i++) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
 $(document).ready(function() {
-    var pattern = [];
-    gameStart = false;
-    nextMove = Math.floor(Math.random()*4)
-    $("btn-play").on("click", function() {
+    var gameStart = false;
+    var nextMove = Math.floor(Math.random()*4)
+    $(".btn-play").on("click", function() {
         pattern = [];
         gameStart = true;
-        nextMove = Math.floor(Math.random()*4)
+        addToPattern();
     });
-    if (gameStart == true) {
-        $("btn-main").on("click", function() {
-            
-        });
-    }
-    
+
+    $(".btn-main").on("click", function() {
+        if (gameStart === true) {
+            clickPattern.push(buttons.indexOf($(this).attr("id")));
+            if (areArraysEqual(pattern, clickPattern)) {
+                addToPattern();
+                clickPattern = [];
+            }
+        }
+    });
 });
